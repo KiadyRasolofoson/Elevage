079763097be88fb0f6288f2ad281088abb57097b
Sylvio3365 <sylvioandri@gmail.com>
2025-02-03 15:05:57 +0300

    mvc

---
+++ app/models/AdminModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\models;
+
+use flight\Engine;
+
+class AdminModel
+{
+    public $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function verifierLogin($email, $mdp)
+    {
+        $sql = "SELECT * FROM admin WHERE email = :email LIMIT 1";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['email' => $email]);
+        $admin = $stmt->fetch(\PDO::FETCH_ASSOC);
+        if ($admin && $mdp == $admin['password']) {
+            return true;
+        }
+        return false;
+    }
+}
