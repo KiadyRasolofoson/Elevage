e2eb64d74b8be4d5ec96c98e291b95102324a828
Sylvio3365 <sylvioandri@gmail.com>
2025-02-03 18:14:29 +0300

    oke

--- app/controllers/WelcomeController.php
+++ app/controllers/WelcomeController.php
@@ -5,39 +5,14 @@ namespace app\controllers;
 use app\models\ProductModel;
 use Flight;
 
-class WelcomeController {
+class WelcomeController
+{
 
-	public function __construct() {
+    public function __construct() {}
 
-	}
-
-	public function home() {
-		$produit = Flight::productModel()->getProduit();
-        $data = ['nom' => $produit["nom"], 'prix'=> $produit["prix"]];
+    public function home()
+    {
+        $data = ['nom' => "oke"];
         Flight::render('welcome', $data);
-    }
-	public function homeDB() {
-		$produit = Flight::productModel()->test();
-        $data = ['nom' => $produit["nom"], 'prix'=> $produit["date_naissance"]];
-        Flight::render('welcome', $data);
-    }
-
-    public function testDB() {
-        $productModel = new ProductModel(Flight::db());
-		$produit = $productModel->test();
-        $data = ['nom' => $produit["nom"], 'prix'=> $produit["date_naissance"]];
-        Flight::render('welcome', $data);
-    }
-
-    //pour tester le template
-    public function homeTemplate() {
-        $data = ['page' => "home"];
-        Flight::render('template', $data);
-    }
-
-    //pour tester le template
-    public function productTemplate() {
-        $data = ['page' => "product"];
-        Flight::render('template', $data);
     }
 }
