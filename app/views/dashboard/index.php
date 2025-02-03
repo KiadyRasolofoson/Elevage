914c9db51e3200b0cf14b1024ed217863e90a1af
KiadyRasolofoson <kiadyrasolofoson11@gmail.com>
2025-02-03 20:48:15 +0300

    modif index

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,12 +1,20 @@
+<?php
+$base_url = Flight::app()->get('flight.base_url');
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 </head>
+
 <body>
-    <?php include('../layout/header.php'); ?>
-    <p>dashboard</p>
+    <?php include('app/views/layout/header.php'); ?>
+    <div class="container">
+        <h1>Dashboard</h1>
+    </div>
 </body>
+
 </html>
