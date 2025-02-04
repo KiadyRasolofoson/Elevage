69df17f05f92d47c24cf37f5eaf72e8612146a49
Sylvio3365 <sylvioandri@gmail.com>
2025-02-04 07:19:38 +0300

    Update index.php

--- app/views/nourriture/index.php
+++ app/views/nourriture/index.php
@@ -1,15 +1,21 @@
-<?php 
+<?php
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acheter nourriture</title>
 </head>
+
 <body>
-    
+    <?php include('app/views/layout/header.php'); ?>
+    <div class="container">
+        
+    </div>
 </body>
+
 </html>
